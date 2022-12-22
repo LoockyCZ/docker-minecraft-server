b49e4400aa809106449ccdafac0a6b8e65ceba68
Lukáš Wagner <lukaswagner1@gmail.com>
2022-12-22 20:58:23 +0100

    Fix stopping a suspended server

--- files/auto/stop.sh
+++ files/auto/stop.sh
@@ -5,6 +5,7 @@ if isTrue "${DEBUG_AUTOSTOP}"; then
   set -x
 fi
 
+/auto/resume.sh stop
 logAutostopAction "Stopping Java process"
 if isTrue "${AUTOSTOP_PKILL_USE_SUDO:-false}"; then
   sudo pkill -f --signal SIGTERM mc-server-runner
